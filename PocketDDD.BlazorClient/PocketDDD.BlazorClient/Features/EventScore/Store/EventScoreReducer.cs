802c7be0d2b07440e65bcb435bf901407a6b7d56
Ross Scott <ross.d.scott@gmail.com>
2023-04-23 08:14:50 +0100

    Remove redundant reducer

--- PocketDDD.BlazorClient/PocketDDD.BlazorClient/Features/EventScore/Store/EventScoreReducer.cs
+++ PocketDDD.BlazorClient/PocketDDD.BlazorClient/Features/EventScore/Store/EventScoreReducer.cs
@@ -8,8 +8,4 @@ public static class EventScoreReducer
     [ReducerMethod]
     public static EventScoreState OnSetEventScore(EventScoreState state, SetEventScoreAction action) =>
         state with { Score = action.Score };
-
-    [ReducerMethod]
-    public static EventScoreState OnEventScoreUpdated(EventScoreState state, EventScoreUpdatedAction action) =>
-        state with { Score = action.Score };
 }
